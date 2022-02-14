3fcc76ce22984e359b2abbc06a75dd726c8b4aad
Timon Emken <timon.emken@fysik.su.se>
2022-02-14 12:03:36 +0100

    New final state class

--- include/DarkART/Wavefunctions_Final.hpp
+++ include/DarkART/Wavefunctions_Final.hpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <vector>
 
+#include "libphysica/Numerics.hpp"
+
+#include "DarkART/Wavefunctions_Initial.hpp"
+
 namespace DarkART
 {
 // 1. Base class for final state wave function
@@ -18,7 +22,7 @@ class Final_Electron_State
 	virtual Final_Electron_State* Clone() const;
 };
 
-// 2. Positive energy continuum solution of Schroedinger equation with hydrogenic potential
+// 2. Positive energy continuum solution of Schroedinger equation with hydrogenic potential (i.e. constant Z_eff)
 extern double Radial_Wavefunction_Hydrogenic(double k_final, unsigned l_prime, double Z_eff, double r);
 
 class Final_Electron_State_Hydrogenic : public Final_Electron_State
@@ -31,6 +35,28 @@ class Final_Electron_State_Hydrogenic : public Final_Electron_State
 	virtual double Radial_Wavefunction(double r, double k_final, unsigned int l_final) override;
 
 	virtual Final_Electron_State_Hydrogenic* Clone() const override;
+};
+
+// 3. Positive energy continuum solution of Schroedinger equation for a given potential Z_eff(r)
+class Final_Electron_State_Schroedinger : Final_Electron_State
+{
+  protected:
+	Initial_Electron_State initial_state;
+	double r_min, r_max;
+	libphysica::Interpolation Z_effective_interpolation;
+
+  public:
+	Final_Electron_State_Schroedinger(Initial_Electron_State& ini_state, double Z_eff = 1.0);
+
+	double Z_effective(double r);
+
+	void Determine_Z_effective();
+
+	void Solve_Schroedinger_Equation();
+
+	virtual double Radial_Wavefunction(double r, double k_final, unsigned int l_final) override;
+
+	virtual Final_Electron_State_Schroedinger* Clone() const override;
 };
 
 }	// namespace DarkART
--- src/Wavefunctions_Final.cpp
+++ src/Wavefunctions_Final.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 #include "libphysica/Natural_Units.hpp"
+#include "libphysica/Utilities.hpp"
 
 #include "DarkART/Special_Functions.hpp"
 #include "DarkART/version.hpp"
@@ -12,13 +13,12 @@ namespace DarkART
 using namespace libphysica::natural_units;
 
 // 1. Base class for final state wave function
-
 Final_Electron_State* Final_Electron_State::Clone() const
 {
 	return new Final_Electron_State(*this);
 }
 
-// 2. Positive energy continuum solution of Schroedinger equation with hydrogenic potential
+// 2. Positive energy continuum solution of Schroedinger equation with hydrogenic potential (i.e. constant Z_eff)
 double Radial_Wavefunction_Hydrogenic(double k_final, unsigned l_final, double Z_eff, double r)
 {
 	double eta = -Z_eff / k_final / Bohr_Radius;
@@ -41,4 +41,32 @@ Final_Electron_State_Hydrogenic* Final_Electron_State_Hydrogenic::Clone() const
 	return new Final_Electron_State_Hydrogenic(*this);
 }
 
+// 3. Positive energy continuum solution of Schroedinger equation for a given potential Z_eff(r)
+
+Final_Electron_State_Schroedinger::Final_Electron_State_Schroedinger(Initial_Electron_State& ini_state, double Z_eff)
+: initial_state(ini_state), r_min(0.0), r_max(50.0 * Bohr_Radius)
+{
+	std::vector<double> r_list = libphysica::Linear_Space(r_min, r_max, 5);
+	std::vector<double> Z_eff_list(r_list.size(), Z_eff);
+	Z_effective_interpolation = libphysica::Interpolation(r_list, Z_eff_list);
+}
+
+double Final_Electron_State_Schroedinger::Z_effective(double r)
+{
+	return Z_effective_interpolation(r);
+}
+
+void Final_Electron_State_Schroedinger::Determine_Z_effective()
+{
+}
+
+double Final_Electron_State_Schroedinger::Radial_Wavefunction(double r, double k_final, unsigned int l_final)
+{
+	return 0.0;
+}
+
+Final_Electron_State_Schroedinger* Final_Electron_State_Schroedinger::Clone() const
+{
+	return new Final_Electron_State_Schroedinger(*this);
+}
 }	// namespace DarkART
