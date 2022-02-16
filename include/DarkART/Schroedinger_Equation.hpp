762179bbf951b7bb49b4507d02a5f85badc724be
Timon Emken <timon.emken@fysik.su.se>
2022-02-16 12:45:42 +0100

    WIP: Implement the Schroedinger equation. Does not work yet, solution does not fit the analytic solution

--- include/DarkART/Schroedinger_Equation.hpp
+++ include/DarkART/Schroedinger_Equation.hpp
@@ -22,22 +22,7 @@ class Schroedinger_Solver
 
 	libphysica::Interpolation Solve_Radial_Equation();
 
-	void operator()(const state_type& x, state_type& dxdt, const double /* t */);
-};
-
-struct push_back_state_and_time
-{
-	std::vector<state_type>& m_states;
-	std::vector<double>& m_times;
-
-	push_back_state_and_time(std::vector<state_type>& states, std::vector<double>& times)
-	: m_states(states), m_times(times) {}
-
-	void operator()(const state_type& x, double t)
-	{
-		m_states.push_back(x);
-		m_times.push_back(t);
-	}
+	void operator()(const state_type& x, state_type& dxdt, const double t);
 };
 
 }	// namespace DarkART
--- src/Schroedinger_Equation.cpp
+++ src/Schroedinger_Equation.cpp
@@ -8,13 +8,27 @@
 namespace DarkART
 {
 using namespace libphysica::natural_units;
-using namespace boost::numeric::odeint;
+
+struct push_back_state_and_time
+{
+	std::vector<state_type>& m_states;
+	std::vector<double>& m_times;
+
+	push_back_state_and_time(std::vector<state_type>& states, std::vector<double>& times)
+	: m_states(states), m_times(times) {}
+
+	void operator()(const state_type& x, double t)
+	{
+		m_states.push_back(x);
+		m_times.push_back(t);
+	}
+};
 
 Schroedinger_Solver::Schroedinger_Solver(double k, unsigned int l)
 : k_final(k), l_final(l)
 {
 	Z_effective = libphysica::Interpolation(
-		libphysica::Log_Space(1e-4 * Bohr_Radius, 51.0 * Bohr_Radius, 5),
+		libphysica::Linear_Space(0.0, 51.0 * Bohr_Radius, 5),
 		std::vector<double>(5, 1.0));
 };
 
@@ -23,28 +37,29 @@ libphysica::Interpolation Schroedinger_Solver::Solve_Radial_Equation()
 	std::vector<state_type> x_vec;
 	std::vector<double> times;
 	state_type x(2);
-	x[0]					 = 1.0;	  // start at x=1.0, p=0.0
-	x[1]					 = 0.0;
-	double t_ini			 = 0.0;
-	double t_final			 = 40.0;
-	double initial_step_size = 0.1;
+	x[0]					 = 0.0;
+	x[1]					 = 0.01;
+	double t_ini			 = 1e-4 * Bohr_Radius;
+	double t_final			 = 10.0 * Bohr_Radius;
+	double initial_step_size = 0.01 * Bohr_Radius;
 
-	size_t steps = integrate(*this,
-							 x, t_ini, t_final, initial_step_size,
-							 push_back_state_and_time(x_vec, times));
-
-	for(size_t i = 0; i <= steps; i++)
-		std::cout << times[i] << '\t' << x_vec[i][0] << '\t' << x_vec[i][1] << std::endl;
+	boost::numeric::odeint::integrate(*this,
+									  x, t_ini, t_final, initial_step_size,
+									  push_back_state_and_time(x_vec, times));
 	std::vector<double> R_list;
 	for(auto& x : x_vec)
 		R_list.push_back(x[0]);
 	return libphysica::Interpolation(times, R_list);
 }
 
-void Schroedinger_Solver::operator()(const state_type& x, state_type& dxdt, const double /* t */)
+void Schroedinger_Solver::operator()(const state_type& x, state_type& dxdt, const double t)
 {
-	dxdt[0] = x[1];
-	dxdt[1] = -x[0] - k_final * x[1];
+	double mu	 = mElectron;
+	double E	 = k_final * k_final / 2.0 / mu;
+	double e	 = Elementary_Charge;
+	double Z_eff = 1.0;
+	dxdt[0]		 = x[1];
+	dxdt[1]		 = -2.0 * x[1] / t + (-2.0 * mu * Z_eff * e * e / t + l_final * (l_final + 1.0) / t / t - 2.0 * mu * E) * x[0];
 }
 
 }	// namespace DarkART
