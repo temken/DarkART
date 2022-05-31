517a72515fecd49a78535beffda3dbeae964b31b
Timon Emken <timon.emken@fysik.su.se>
2022-05-31 10:18:02 +0200

    Add unit tests to check the normalization of each atomic shell included in /data

--- tests/test_Wavefunctions_Initial.cpp
+++ tests/test_Wavefunctions_Initial.cpp
@@ -59,7 +59,95 @@ TEST(TestWavefunctions, TestRadialWavefunctionDerivative)
 			EXPECT_LT(electron.Radial_Wavefunction(r + eps), electron.Radial_Wavefunction(r));
 }
 
-TEST(TestWavefunctions, TestNormalization)
+TEST(TestWavefunctions, TestNormalizationArgon)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Ar", 3, 1),
+		Initial_Electron_State("Ar", 3, 0),
+		Initial_Electron_State("Ar", 2, 1),
+		Initial_Electron_State("Ar", 2, 0),
+		Initial_Electron_State("Ar", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationGermanium)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Ge", 4, 1),
+		Initial_Electron_State("Ge", 4, 0),
+		Initial_Electron_State("Ge", 3, 2),
+		Initial_Electron_State("Ge", 3, 1),
+		Initial_Electron_State("Ge", 3, 0),
+		Initial_Electron_State("Ge", 2, 1),
+		Initial_Electron_State("Ge", 2, 0),
+		Initial_Electron_State("Ge", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationIodine)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("I", 5, 1),
+		Initial_Electron_State("I", 5, 0),
+		Initial_Electron_State("I", 4, 2),
+		Initial_Electron_State("I", 4, 1),
+		Initial_Electron_State("I", 4, 0),
+		Initial_Electron_State("I", 3, 2),
+		Initial_Electron_State("I", 3, 1),
+		Initial_Electron_State("I", 3, 0),
+		Initial_Electron_State("I", 2, 1),
+		Initial_Electron_State("I", 2, 0),
+		Initial_Electron_State("I", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationSodium)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Na", 3, 0),
+		Initial_Electron_State("Na", 2, 1),
+		Initial_Electron_State("Na", 2, 0),
+		Initial_Electron_State("Na", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationSilicon)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Si", 3, 1),
+		Initial_Electron_State("Si", 3, 0),
+		Initial_Electron_State("Si", 2, 1),
+		Initial_Electron_State("Si", 2, 0),
+		Initial_Electron_State("Si", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationXenon)
 {
 	// ARRANGE
 	double tol									  = 1.0e-5;
@@ -75,11 +163,6 @@ TEST(TestWavefunctions, TestNormalization)
 		Initial_Electron_State("Xe", 2, 1),
 		Initial_Electron_State("Xe", 2, 0),
 		Initial_Electron_State("Xe", 1, 0),
-		Initial_Electron_State("Ar", 3, 1),
-		Initial_Electron_State("Ar", 3, 0),
-		Initial_Electron_State("Ar", 2, 1),
-		Initial_Electron_State("Ar", 2, 0),
-		Initial_Electron_State("Ar", 1, 0),
 	};
 	// ACT & ASSERT
 	for(auto& electron : electrons)
