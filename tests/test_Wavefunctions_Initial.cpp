b0aee3df4924b5ecf3d02e44a164295a4aef92f6
Timon Emken <timon.emken@fysik.su.se>
2022-10-25 11:11:20 +0200

    Add momentum space wavefunctions

--- tests/test_Wavefunctions_Initial.cpp
+++ tests/test_Wavefunctions_Initial.cpp
@@ -183,6 +183,130 @@ TEST(TestWavefunctions, TestNormalizationXenon)
 		EXPECT_NEAR(electron.Normalization(), 1.0, tol);
 }
 
+TEST(TestWavefunctions, TestNormalizationCarbonMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("C", 2, 1),
+		Initial_Electron_State("C", 2, 0),
+		Initial_Electron_State("C", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationArgonMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Ar", 3, 1),
+		Initial_Electron_State("Ar", 3, 0),
+		Initial_Electron_State("Ar", 2, 1),
+		Initial_Electron_State("Ar", 2, 0),
+		Initial_Electron_State("Ar", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationGermaniumMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Ge", 4, 1),
+		Initial_Electron_State("Ge", 4, 0),
+		Initial_Electron_State("Ge", 3, 2),
+		Initial_Electron_State("Ge", 3, 1),
+		Initial_Electron_State("Ge", 3, 0),
+		Initial_Electron_State("Ge", 2, 1),
+		Initial_Electron_State("Ge", 2, 0),
+		Initial_Electron_State("Ge", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationIodineMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("I", 5, 1),
+		Initial_Electron_State("I", 5, 0),
+		Initial_Electron_State("I", 4, 2),
+		Initial_Electron_State("I", 4, 1),
+		Initial_Electron_State("I", 4, 0),
+		Initial_Electron_State("I", 3, 2),
+		Initial_Electron_State("I", 3, 1),
+		Initial_Electron_State("I", 3, 0),
+		Initial_Electron_State("I", 2, 1),
+		Initial_Electron_State("I", 2, 0),
+		Initial_Electron_State("I", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationSodiumMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Na", 3, 0),
+		Initial_Electron_State("Na", 2, 1),
+		Initial_Electron_State("Na", 2, 0),
+		Initial_Electron_State("Na", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationSiliconMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Si", 3, 1),
+		Initial_Electron_State("Si", 3, 0),
+		Initial_Electron_State("Si", 2, 1),
+		Initial_Electron_State("Si", 2, 0),
+		Initial_Electron_State("Si", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
+TEST(TestWavefunctions, TestNormalizationXenonMomentum)
+{
+	// ARRANGE
+	double tol									  = 1.0e-5;
+	std::vector<Initial_Electron_State> electrons = {
+		Initial_Electron_State("Xe", 5, 1),
+		Initial_Electron_State("Xe", 5, 0),
+		Initial_Electron_State("Xe", 4, 2),
+		Initial_Electron_State("Xe", 4, 1),
+		Initial_Electron_State("Xe", 4, 0),
+		Initial_Electron_State("Xe", 3, 2),
+		Initial_Electron_State("Xe", 3, 1),
+		Initial_Electron_State("Xe", 3, 0),
+		Initial_Electron_State("Xe", 2, 1),
+		Initial_Electron_State("Xe", 2, 0),
+		Initial_Electron_State("Xe", 1, 0),
+	};
+	// ACT & ASSERT
+	for(auto& electron : electrons)
+		EXPECT_NEAR(electron.Normalization_Momentum(), 1.0, tol);
+}
+
 TEST(TestWavefunctions, TestPrintSummary)
 {
 	// ARRANGE
